3d6a518697c1f1bfd691452394586e7cead02dde
Yash-KK <reachyashk@gmail.com>
2023-04-05 06:53:08 +0530

    built REST API

--- questions/serializer.py
+++ questions/serializer.py
@@ -12,7 +12,7 @@ class QuestionSerializer(serializers.ModelSerializer):
     user_has_answered = serializers.SerializerMethodField()
     created_at = serializers.SerializerMethodField()
     slug = serializers.SlugField(read_only=True)
-
+ 
     class Meta:
         model = Question
         exclude = ['id','modified_at']
@@ -26,3 +26,28 @@ class QuestionSerializer(serializers.ModelSerializer):
     def get_user_has_answered(self,obj):
         authenticated_user = self.context.get('request').user
         return obj.answers.filter(author=authenticated_user).exists()
+
+class AnswerSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    likes_count = serializers.SerializerMethodField()
+    question_slug = serializers.SerializerMethodField()
+    user_has_liked = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Answer
+        exclude = ['modified_at', 'question', 'voters']
+
+    def get_created_at(self, obj):
+        return obj.created_at.strftime('%Y-%m-%d')    
+    
+    def get_likes_count(self, obj):
+        return obj.voters.count()
+    
+    def get_question_slug(self, obj):
+        return obj.question.slug
+
+    def get_user_has_liked(self, obj):
+        authenticated_user = self.context.get('request').user
+        return obj.voters.filter(pk=authenticated_user.pk).exists()
+    
--- questions/urls.py
+++ questions/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
 from .views import (
-    QuestionListAPI
+    QuestionListAPI,
+    QuestionDetailAPI,
+
+    AnswerCreateAPI,
+    AnswerListAPI,
+    AnswerDetailAPI,
+
+    AnswerLikeAPI
 )
 urlpatterns = [
-    path('questions/', QuestionListAPI.as_view(),name='question-list')
+    path('questions/', QuestionListAPI.as_view(),name='question-list'),
+    path('questions/<slug:slug>/', QuestionDetailAPI.as_view(), name='question-detail'),
+
+    path('questions/<slug:slug>/answer-create/', AnswerCreateAPI.as_view(), name='answer-create'),
+    path('questions/<slug:slug>/answers/', AnswerListAPI.as_view(), name='answer-list'),
+
+    path('answers/<int:pk>/',AnswerDetailAPI.as_view(), name='answer-detail'),
+    path('answers/<int:pk>/like/',AnswerLikeAPI.as_view(), name='answer-like')
+
 ]
